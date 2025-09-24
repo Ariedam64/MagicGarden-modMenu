74538169e10f306436414fc851a73b8ca1e6cb32
Ariedam64 <75644109+Ariedam64@users.noreply.github.com>
2025-09-24 18:45:22 +0200

    Add files via upload

--- quinoa-ws.min.user.js
+++ quinoa-ws.min.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Magic Garden ModMenu 
 // @namespace    Quinoa
-// @version      1.1.3
+// @version      1.1.4
 // @match        https://*.discordsays.com/*
 // @match        https://magiccircle.gg/r/*
 // @match        https://magicgarden.gg/r/*
