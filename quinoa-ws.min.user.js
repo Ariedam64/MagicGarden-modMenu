6c4febd89a1bfe4bb324e4f9ef186c462d167a03
Ariedam64 <75644109+Ariedam64@users.noreply.github.com>
2025-10-08 05:09:56 +0200

    Add files via upload

--- quinoa-ws.min.user.js
+++ quinoa-ws.min.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Arie's Mod
 // @namespace    Quinoa
-// @version      1.9.3
+// @version      1.9.4
 // @match        https://1227719606223765687.discordsays.com/*
 // @match        https://magiccircle.gg/r/*
 // @match        https://magicgarden.gg/r/*
